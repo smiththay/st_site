4d7b5b2452816bd4cb1f21490d4f49e9cd46547a
Smith Thay <contact.smith.thay@gmail.com>
2021-05-19 19:39:35 -0400

    button added not functional

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,17 @@
 import React from 'react'
-import './Landing.css';
+import './Landing.css'
+import { Link } from 'react-router-dom'
 
 export default function Landing() {
     return (
         <div className='background'>
+        <div className='name'>
+          <Link to='' style={{color: 'white', textDecoration: 'none'}} ><b>smith.</b></Link>
         </div>
+        <button class="btn third">Join Me</button>
+   
+        </div>
+
+        
     )
 }
