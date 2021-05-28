db3a658e60c796981b3ebc6445a77c67f3307ed6
Smith Thay <contact.smith.thay@gmail.com>
2021-05-27 22:10:50 -0400

    added blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -5,9 +5,86 @@ import Footer from './Footer'
 export default function Blog() {
     return (
         <div>
-        <Navbar />
-            hello
-        <Footer />
+            <Navbar />
+            <div class="container mt-5 pt-5 px-5">
+
+                <div class="row pt-5">
+                    <div class="col pt-5">
+                        <h3>Week Five "The One About Bootstrap"</h3>
+
+                        <p>The biggest thing that held me back this week is how well I utilized my time this week after I left boot camp. What I mean is I am trying to figure out how to structure my day to best utilize my energy and not my time.</p>
+
+                        <p>To be quite frank, JavaScript is still very confusing because its so complex and has so many layers, it has been challenging to grasp it. It makes it difficult for me to process also because there is so many other things that we are learning as well.</p>
+
+                        <p>I thought Bootstrap and Atomic Design was awesome to learn this week. After getting used to it, it made structuring and styling a webpage so much easier. It simplifies so much HTML and CSS. I would like to learn more about how some of these larger websites and applications have utilized bootstrap for their sites. </p>
+                    </div>
+                </div>
+            
+
+            <div class="row pt-5">
+                <div class="col pt-5">
+                    <h3>Week Four "The One About More Javascript"</h3>
+
+                    <p>Each week had its own challenges, but with every challenge came an "AHA moment". There was a lot to dislike about learning any new topic because it is challenging. Git and Github was very confusing at first because it seemed useless to do all these steps to complete a task as simple as saving progress. But once I figured out how to do it, its purpose became more clear. It also saved me when making mistake, I was able to go back and start from a spot it was functioning. HTML was pretty simple to pick up because I had previous experience. CSS was new but was pretty easy to figure out. Javascript was a whole different beast though. It was so challenging to figure out syntax, how to properly work out functions, and generally just get code to work. </p>
+
+                    <p>What is helpful to me is to just get down to business and get hands on with the projects. The reading content is fine but I learn best when I can just get  down to the nitty gritty and go through the trials of working these project out.</p>
+
+                    <p>What hindered my progress is purely just being an absolute noob to all the topics we learned. With the exception of HTML, I had never messed with any of the languages. JavaScript and the Katas were especially difficult. </p>
+
+                    <p>For me the biggest way to overcome challenges is to remember my why. I am doing this boot camp for bigger reasons than just what is surface level. I have to remember my time is limited and I want to use it well. </p>
+
+                </div>
+            </div>
+
+
+            <div class="row pt-5">
+                <div class="col pt-5">
+                    <h3>Week Three "The One About JavaScript"</h3>
+
+                    <p>Back to the cliff of confusion. JavaScript has been very challenging. It is just vast and complex, and there is so much that you can do with the language. I can see how connecting JavaScript, HTML, and CSS are so fundamental in building websites and applications. By being proficient in all three can drastically change the user experience. </p>
+
+                    <p>So far this blog incorporates just some HTML but later on in the bootcamp, I hope to incorporate CSS and JavaScript to take this blog to another level. Stay tuned to find out. </p>
+
+                    <p>My thoughts on pseudo coding have completely 180'd. Pseudo coding can make a problem or project drastically easier. It takes more effort to create a good pseudo code than I initially thought. I will need to practice some more to really grasp pseudo coding for what it can do for my coding. </p>
+                </div>
+            </div>
+
+
+            <div class="row pt-5">
+                <div class="col pt-5">
+                    <h3> Week Two "The One About HTML"</h3>
+
+                    <p><i>Sigh of relief...</i></p>
+
+                    <p>Some thing seems familiar for the first time in bootcamp, albeit it has been many years since I have really messed with it. HTML, I believe most people have had to mess with it in a computer science class in highschool. It wasn't as difficult to get back in it compared to last week learning completely new topics. We worked on a *Markup Letter*, *Structure a Page of Content*, and our demo day project, *Layout a Splash Page*. I can see how pseudo coding can be very beneficial before going into a project. I hope to develop better practice in the weeks to come. </p>
+
+                    <p>I struggled with less this week but that doesn't mean I didn't struggle at all. Along side HTML, we also dove in on CSS. There were many new elements in HTML that I discovered, and everything in CSS was new. All the projects didn't require too much CSS knowledge so I didn't have to worry all that much about learning CSS syntax. I struggled a bit with the resizing the panda picture in the splash page project. As many of the struggles I had, it was a small detail.</p>
+
+                    <p>Im eager to learn more about CSS. I want to flex my creative side some more and being good at CSS will definitely help with that. In addition to CSS, I am excited, and nervous, to learn Javscript. </p>
+                </div>
+            </div>
+
+
+
+
+            <div class="row pt-5">
+                <div class="col">
+
+                    <h3>Week One "The One About Git"</h3>
+
+                    <p>I came in with expectations to struggle and Awesome Inc. most certainly went above and beyond. Coming from a background unrelated to tech, I felt I was behind the 8 ball. Everything was new. The good news is that you can’t become great by taking the easy route. The expectation of 15-20 hours a week was not so true for me, as I put in extra hours to understand Terminal, VS Code, Git, and Github.</p>
+
+                    <p>So what am I struggling with? The answer is <strong>YES</strong>. As I said, everything is new. So like learning to ride a bike, you’re going to get a lot of scrapes and bruises. I first struggled with the concepts of terminal, git, and GitHub. Then I struggled with how to be technical with them as I tried to see how they all interfaced. Started to get some traction to only be derailed again when starting the blog. As I am working on this blog, I am solidifying many of the concepts and processes that I had learned throughout the week. </p>
+
+                    <p>This whole journey is exciting. Im eager and optimistic to learn more as I progress in the program. Currently, Im excited to have git and Github down so well that I can look back at this blog and laugh at the way I struggled. I can’t wait to fail next week.</p>
+
+                </div>
+            </div>
+
+        </div>
+
+
+            <Footer />
         </div>
     )
 }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,12 +1,34 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import './Portfolio.css'
 
 export default function Portfolio() {
+
     return (
         <div>
-        <Navbar />
-            <h1>nooooooo</h1>hello
+            <Navbar />
+            <div className='container text-center mt-5 pt-5'>
+                <div className='row pt-5'>
+                    <div className='col-md-6 col-sm-12 pt-2'>
+                        <div className="card-list">
+
+                            <div className='row d-flex justify-content-center mt-3'>
+                                <div className='col-6 border-danger'>
+                                    <h2 className="card-title"><b>Sapling</b></h2>
+                                    <span><b>Social Budgeting App</b></span>
+                                </div>
+                            </div>
+                            <div className="card-body ">
+
+                                <p className="card-text">Description</p>
+                            </div>
+                            <h2></h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+       
             <Footer />
         </div>
     )
